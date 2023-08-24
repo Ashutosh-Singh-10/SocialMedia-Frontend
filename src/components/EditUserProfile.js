2d34d5c145abd69305e5fa6da311fe5b06900cf6
JaiShankar <jaishankar.a1603@gmail.com>
2023-08-24 23:16:36 +0530

    Fix:small

--- src/components/EditUserProfile.js
+++ src/components/EditUserProfile.js
@@ -1,12 +1,23 @@
+import axios from 'axios';
 import React from 'react'
 
 const EditUserProfile = () => {
+    const url = process.env.REACT_APP_BACKEND_URL;
+    const url1 = url + '/profile/update';
+    const handleUserProfileUpdate = (e) => {
+        e.stopPropagation();
+        const res = axios.post(url1, {
+
+        })
+    }
     return (
         <>
-            <form>
-                <input type='text' placeholder='Name' />
-                <input type='text' />
-
+            <form onSubmit={handleUserProfileUpdate}>
+                <input type="file" id="img" name="img" accept="image/*" />
+                <input type='text' placeholder='First Name' />
+                <input type='text' placeholder='Last Name' />
+                <textarea rows={4} cols={30} />
+                <input type='submit' placeholder='Submit' />
             </form>
         </>
     )
