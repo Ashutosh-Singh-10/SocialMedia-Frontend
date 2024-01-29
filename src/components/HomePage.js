9d17be61c2f66901527ae393e6d861dd061bcd9a
Ashutosh Singh <ashutoshsinghas24092002@gmail.com>
2024-01-29 17:48:49 +0530

    Design: change

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -147,32 +147,26 @@ const HomePage = () => {
 
 
                 <div className='hl-mbx'>
-                    <div className="hl-mcn1 flexVC">
-                        <div className='flexCenter'>
-                            <img src={require("../assets/images/sheldon.png")} className="hl-img" alt="" />
-                        </div>
-                        <div className='hl-f1'>
-                            Sheldon Cooper
-                        </div>
-                        <hr className='w90' />
-                        <div className='w85 hl-f2'>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt dolorem sequi animi officiis fugiat quis aut consequatur tempore eos fuga numquam consequuntur nam unde laboriosam sunt velit magnam in amet enim consectetur, quaerat, maiores beatae nemo. Necessitatibus perspiciatis assumenda in!
-                        </div>
 
+                    <div className=' flexVC'>
+
+
+                            <div className='w80 '>
+                                <div className='w100 textS'>
+
+                        Menu
+                                </div>
+                        <div className=' flexVC  w100'>
+                            <div className='w80 b textS'>home</div>
+                            <div className='w80 b textS'>home</div>
+                            <div className='w80 b textS'>home</div>
+                            
+                            
+                        </div>
+                            </div>
+                      
                     </div>
-                    <div className="hl-mcn2 flexVC w100">
-                        <div className="flexCenter w100 hl-f2 hl-bx1">
-
-                            <div className="w33 ">24 Posts</div>
-                            <div className="w33 hl-l-bd">100 Followers</div>
-                            <div className="w33 hl-l-bd">100 Following</div>
-
-                        </div>
-                        <div className="flexCenter w100 hl-bx2">
-                            My Save
-
-                        </div>
-                    </div>
+               
 
                 </div>
 
@@ -235,21 +229,31 @@ const HomePage = () => {
 
                         <div className=" w100 hr-cn1">
                             &ensp;&ensp;&nbsp;
-                            Recent Following
+                            People you might know
 
                         </div>
 
                         {Activity1Data?.data?.map((element, id) => {
 
                             return (
-                                <Link to={`/users/${element?.username}`} className='w100 hr-cn2' key={id}>
+                                <div  className='w100 hr-cn2' key={id}>
+                                    <div className='flexCenter'>
+
                                     <img src={`${url}${element.avatar}`} className="hr-img" alt="" />
-                                    <div>{element.username}</div>
-                                </Link>
+                                    <Link to={`/users/${element?.username}`}  className='hr-fn-1'>{element.username}</Link>
+                                    </div>
+                                    <div >
+                                      <button class="hr-fl-btn">Follow</button>
+
+                                    </div>
+                                </div>
 
 
                             );
                         })}
+                        <div> &nbsp; </div>
+                        <div> See more </div>
+                        <div> &nbsp; </div>
 
 
                         {/* <div className='w100 hr-cn2'>
@@ -278,10 +282,20 @@ const HomePage = () => {
                         {Activity2Data?.data?.map((element, id) => {
 
                             return (
-                                <Link to={`/users/${element?.username}`} className='w100 hr-cn2' key={id}>
-                                    <img src={`${url}${element.avatar}`} className="hr-img" alt="" />
-                                    <div>{element.username}</div>
-                                </Link>
+           
+                                
+
+                            <div  className='w100 hr-cn2' key={id}>
+                            <div className='flexCenter'>
+
+                            <img src={`${url}${element.avatar}`} className="hr-img" alt="" />
+                            <Link to={`/users/${element?.username}`}  className='hr-fn-1'>{element.username}</Link>
+                            </div>
+                            <div >
+                            <button class="hr-fl-btn">Follow</button>
+
+                            </div>
+                            </div>
 
 
                             );
